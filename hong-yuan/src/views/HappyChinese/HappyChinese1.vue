b2097a7ff70ef9bfbc437628f45b42c50916a466
刘丹 <764189132@qq.com>
2024-01-03 17:46:13 +0700

    upload
    upload

--- hong-yuan/src/views/HappyChinese/HappyChinese1.vue
+++ hong-yuan/src/views/HappyChinese/HappyChinese1.vue
@@ -1,5 +1,28 @@
 <script>
 import m1 from '@/assets/happy-audio/1/01 Unit 1 - You And I - Lesson 1 - P. 1.mp3'
+import m2 from '@/assets/happy-audio/1/02 Unit 1 - You And I - Lesson 2 - P. 4.mp3'
+import m3 from '@/assets/happy-audio/1/03 Unit 1 - You And I - Lesson 3 - P. 8.mp3'
+import m4 from '@/assets/happy-audio/1/04 Unit 2 - My Family - Lesson 4 - P. 13.mp3'
+import m5 from '@/assets/happy-audio/1/05 Unit 2 - My Family - Lesson 5 - P. 18.mp3'
+import m6 from '@/assets/happy-audio/1/06 Unit 2 - My Family - Lesson 6 - P. 23.mp3'
+import m7 from '@/assets/happy-audio/1/07 Unit 3 - Food - Lesson 7 - P. 28.mp3'
+import m8 from '@/assets/happy-audio/1/08 Unit 3 - Food - Lesson 8 - P. 32.mp3'
+import m9 from '@/assets/happy-audio/1/09 Unit 3 - Food - Lesson 9 - P. 37.mp3'
+import m10 from '@/assets/happy-audio/1/10 Unit 4 - School Life - Lesson 10 - P. 42.mp3'
+import m11 from '@/assets/happy-audio/1/11 Unit 4 - School Life - Lesson 11 - P. 47.mp3'
+import m12 from '@/assets/happy-audio/1/12 Unit 4 - School Life - Lesson 12 - P. 51.mp3'
+import m13 from '@/assets/happy-audio/1/13 Unit 5 - Time & Weather - Lesson 13 - P. 56.mp3'
+import m14 from '@/assets/happy-audio/1/14 Unit 5 - Time & Weather - Lesson 14 - P. 60.mp3'
+import m15 from '@/assets/happy-audio/1/15 Unit 5 - Time & Weather - Lesson 15 - P. 64.mp3'
+import m16 from '@/assets/happy-audio/1/16 Unit 6 - Job - Lesson 16 - P. 69.mp3'
+import m17 from '@/assets/happy-audio/1/17 Unit 6 - Job - Lesson 17 - P. 74.mp3'
+import m18 from '@/assets/happy-audio/1/18 Unit 6 - Job - Lesson 18 - P. 79.mp3'
+import m19 from '@/assets/happy-audio/1/19 Unit 7 - Hobby - Lesson 19 - P. 85.mp3'
+import m20 from '@/assets/happy-audio/1/20 Unit 7 - Hobby - Lesson 20 - P. 89.mp3'
+import m21 from '@/assets/happy-audio/1/21 Unit 7 - Hobby - Lesson 21 - P. 93.mp3'
+import m22 from '@/assets/happy-audio/1/22 Unit 8 - Transport & Travel - Lesson 22 - P. 99.mp3'
+import m23 from '@/assets/happy-audio/1/23 Unit 8 - Transport & Travel - Lesson 23 - P. 104.mp3'
+import m24 from '@/assets/happy-audio/1/24 Unit 8 - Transport & Travel - Lesson 24 - P. 109.mp3'
 
 export default{
     data() {
@@ -9,6 +32,99 @@ export default{
                     'title':'01 Unit 1 - You And I - Lesson 1 - P. 1.mp3',
                     'src':m1
                 },
+                {
+                    'title':'02 Unit 1 - You And I - Lesson 2 - P. 4.mp3',
+                    'src':m2
+                },
+                {
+                    'title':'03 Unit 1 - You And I - Lesson 3 - P. 8.mp3',
+                    'src':m3
+                },
+                {
+                    'title':'04 Unit 2 - My Family - Lesson 4 - P. 13.mp3',
+                    'src':m4
+                },
+                {
+                    'title':'05 Unit 2 - My Family - Lesson 5 - P. 18.mp3',
+                    'src':m5
+                },
+                {
+                    'title':'06 Unit 2 - My Family - Lesson 6 - P. 23.mp3',
+                    'src':m6
+                },
+                {
+                    'title':'07 Unit 3 - Food - Lesson 7 - P. 28.mp3',
+                    'src':m7
+                },
+                {
+                    'title':'08 Unit 3 - Food - Lesson 8 - P. 32.mp3',
+                    'src':m8
+                },
+                {
+                    'title':'09 Unit 3 - Food - Lesson 9 - P. 37.mp3',
+                    'src':m9
+                },
+                {
+                    'title':'10 Unit 4 - School Life - Lesson 10 - P. 42.mp3',
+                    'src':m10
+                },
+                {
+                    'title':'11 Unit 4 - School Life - Lesson 11 - P. 47.mp3',
+                    'src':m11
+                },
+                {
+                    'title':'12 Unit 4 - School Life - Lesson 12 - P. 51.mp3',
+                    'src':m12
+                },
+                {
+                    'title':'13 Unit 5 - Time & Weather - Lesson 13 - P. 56.mp3',
+                    'src':m13
+                },
+                {
+                    'title':'14 Unit 5 - Time & Weather - Lesson 14 - P. 60.mp3',
+                    'src':m14
+                },
+                {
+                    'title':'15 Unit 5 - Time & Weather - Lesson 15 - P. 64.mp3',
+                    'src':m15
+                },
+                {
+                    'title':'16 Unit 6 - Job - Lesson 16 - P. 69.mp3',
+                    'src':m16
+                },
+                {
+                    'title':'17 Unit 6 - Job - Lesson 17 - P. 74.mp3',
+                    'src':m17
+                },
+                {
+                    'title':'18 Unit 6 - Job - Lesson 18 - P. 79.mp3',
+                    'src':m18
+                },
+                {
+                    'title':'19 Unit 7 - Hobby - Lesson 19 - P. 85.mp3',
+                    'src':m19
+                },
+                {
+                    'title':'20 Unit 7 - Hobby - Lesson 20 - P. 89.mp3',
+                    'src':m20
+                },
+                {
+                    'title':'21 Unit 7 - Hobby - Lesson 21 - P. 93.mp3',
+                    'src':m21
+                },
+                {
+                    'title':'22 Unit 8 - Transport & Travel - Lesson 22 - P. 99.mp3',
+                    'src':m22
+                },
+                {
+                    'title':'23 Unit 8 - Transport & Travel - Lesson 23 - P. 104.mp3',
+                    'src':m23
+                },
+                {
+                    'title':'24 Unit 8 - Transport & Travel - Lesson 24 - P. 109.mp3',
+                    'src':m24
+                },
+
             ],
             song: '',
             playing: false
